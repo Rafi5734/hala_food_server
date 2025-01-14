9651cfd73efb70d21bad4fd67462a049c5cbbfd9
Rafi5734 <kamrulhasun5734@gmail.com>
2025-01-14 19:52:00 +0600

    comment api modified

--- models/products.js
+++ models/products.js
@@ -1,30 +1,34 @@
 const mongoose = require("mongoose");
 const { v4: uuidv4 } = require("uuid");
 
-const productsSchema = new mongoose.Schema({
-  category: { type: String },
-  name: { type: String },
-  imageLink: { type: String },
-  price: { type: Number },
-  description: { type: String },
-  discount: { type: String },
-  quantity: { type: Number },
-  SKUId: { type: String },
-  status: { type: String },
-  stock: { type: String },
-  sold: { type: String },
-  reviews: [
-    {
-      comment_id: {
-        type: String,
-        default: uuidv4,
+const productsSchema = new mongoose.Schema(
+  {
+    category: { type: String },
+    name: { type: String },
+    imageLink: { type: String },
+    price: { type: Number },
+    description: { type: String },
+    discount: { type: String },
+    quantity: { type: Number },
+    SKUId: { type: String },
+    status: { type: String },
+    stock: { type: String },
+    sold: { type: String },
+    reviews: [
+      {
+        comment_id: {
+          type: String,
+          default: uuidv4,
+        },
+        text: { type: String },
       },
-      username: { type: String },
-      text: { type: String },
-    },
-  ],
-  subCategory: { type: String },
-});
+    ],
+    subCategory: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const products = mongoose.model("all_products", productsSchema);
 module.exports = products;
