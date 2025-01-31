ca9bfc87a645a6bbcd46be36e43a1801203d318d
Rafi5734 <kamrulhasun5734@gmail.com>
2025-02-01 02:01:25 +0600

    timestamps added in the order api

--- models/checkout.js
+++ models/checkout.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
 const { v4: uuidv4 } = require("uuid");
 
-const checkoutSchema = new mongoose.Schema({
-  fullName: { type: String },
-  phoneNumber: { type: String },
-  address: { type: String },
-  thanaDistrict: { type: String },
-  note: { type: String },
-  delivery_charge: { type: String },
-  totalPrice: { type: Number },
-  order: [],
-});
+const checkoutSchema = new mongoose.Schema(
+  {
+    fullName: { type: String },
+    phoneNumber: { type: String },
+    address: { type: String },
+    thanaDistrict: { type: String },
+    note: { type: String },
+    delivery_charge: { type: String },
+    totalPrice: { type: Number },
+    order: [],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const checkout = mongoose.model("order_checkout", checkoutSchema);
 module.exports = checkout;
--- models/order.js
+++ models/order.js
@@ -1,39 +1,44 @@
 const mongoose = require("mongoose");
 const { v4: uuidv4 } = require("uuid");
 
-const orderSchema = new mongoose.Schema({
-  category: { type: String },
-  name: { type: String },
-  imageLink: { type: String },
-  imageLinks: [
-    {
-      // productId: { type: String },
-      url: { type: String, required: true }, // Image URL
-      title: { type: String, required: true }, // Optional title
-    },
-  ],
-  color: { type: String },
-  price: { type: Number },
-  description: { type: String },
-  discount: { type: String },
-  quantity: { type: Number },
-  SKUId: { type: String },
-  status: { type: String },
-  stock: { type: String },
-  sold: { type: String },
-  size: { type: String },
-  reviews: [
-    {
-      comment_id: {
-        type: String,
-        default: uuidv4,
+const orderSchema = new mongoose.Schema(
+  {
+    category: { type: String },
+    name: { type: String },
+    imageLink: { type: String },
+    imageLinks: [
+      {
+        // productId: { type: String },
+        url: { type: String, required: true }, // Image URL
+        title: { type: String, required: true }, // Optional title
       },
-      username: { type: String },
-      text: { type: String },
-    },
-  ],
-  subCategory: { type: String },
-});
+    ],
+    color: { type: String },
+    price: { type: Number },
+    description: { type: String },
+    discount: { type: String },
+    quantity: { type: Number },
+    SKUId: { type: String },
+    status: { type: String },
+    stock: { type: String },
+    sold: { type: String },
+    size: { type: String },
+    reviews: [
+      {
+        comment_id: {
+          type: String,
+          default: uuidv4,
+        },
+        username: { type: String },
+        text: { type: String },
+      },
+    ],
+    subCategory: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const orders = mongoose.model("orders", orderSchema);
 module.exports = orders;
